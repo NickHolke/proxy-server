d1a5f2b2fbcc2a1eab2068499e081b1dd55ee765
Nick Holke <nickholke@gmail.com>
2019-12-10 15:18:02 -0800

    Added images for tradestock

--- Proxy-Server/server.js
+++ Proxy-Server/server.js
@@ -11,6 +11,18 @@ app.get('/tradestock/api/10', (req,res) => {
   res.redirect('http://localhost:3002/tradestock/api/10');
 })
 
+app.get('/questionMark.png', (req, res) => {
+  res.redirect('http://localhost:3002/questionMark.png')
+})
+
+app.get('/exclamation.png', (req, res) => {
+  res.redirect('http://localhost:3002/exclamation.png')
+})
+
+app.get('/arrows.png', (req, res) => {
+  res.redirect('http://localhost:3002/arrows.png')
+})
+
 app.get('/ratings/getData/1', (req,res) => {
   res.redirect('http://localhost:3005/ratings/getData/1');
 })
@@ -26,6 +38,7 @@ app.get('/earnings/getData', (req,res) => {
 app.get('/news/getData', (req,res) => {
   res.redirect('http://localhost:3004/news/getData')
 })
+
 
 app.post('/tradestock/user/deposit', (req,res) => {
   res.redirect(307, 'http://localhost:3002/tradestock/user/deposit');
