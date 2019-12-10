fd761e0ea191676c27b11524d357266805c7f740
Nick Holke <nickholke@gmail.com>
2019-12-10 11:23:17 -0800

    Added graph to proxy server

--- Proxy-Server/server.js
+++ Proxy-Server/server.js
@@ -1,31 +1,15 @@
 const express = require('express');
-const request = require('request');
-var cors = require('cors');
 
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3008;
 
-// app.use(cors());
-app.use(bodyParser.json());
+app.get('/graph/getStocks', (req, res) => {
+  res.redirect('http://localhost:3001/graph/getStocks');
+})
 
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//   request('http://localhost:3001/', (err, response, body) => {
-//     if (err) {
-//       res.status(400);
-//       res.send(err);
-//     } else {
-//       console.log(response);
-//       res.send(body)
-//     }
-//   })
-
-//   // request('http://www.google.com', (err, response, body) => {
-//   //   res.send(body)
-//   // })
-// })
 
 app.listen(port, () => {
   console.log('listening on port '+ port);
