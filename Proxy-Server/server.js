76bb139603c740ec163f7bb8b5c6722e1602f8eb
Nick Holke <nickholke@gmail.com>
2019-12-13 09:59:36 -0800

    Changed local host to aws Ip

--- Proxy-Server/server.js
+++ Proxy-Server/server.js
@@ -3,46 +3,69 @@ const express = require('express');
 const app = express();
 const port = 3008;
 
-app.get('/graph/getStocks', (req, res) => {
-  res.redirect('http://localhost:3001/graph/getStocks');
-})
+
+// app.get('/tradestock/api/10', (req,res) => {
+//   res.redirect('http://localhost:3002/tradestock/api/10');
+// })
 
 app.get('/tradestock/api/10', (req,res) => {
-  res.redirect('http://localhost:3002/tradestock/api/10');
+  res.redirect('http://34.214.68.82/tradestock/api/10');
 })
 
-app.get('/questionMark.png', (req, res) => {
-  res.redirect('http://localhost:3002/questionMark.png')
+app.get('/questionMark.png', (req,res) => {
+  res.redirect('http://34.214.68.82/questionMark.png');
 })
 
-app.get('/exclamation.png', (req, res) => {
-  res.redirect('http://localhost:3002/exclamation.png')
+app.get('/exclamation.png', (req,res) => {
+  res.redirect('http://34.214.68.82/exclamation.png');
 })
 
 app.get('/arrows.png', (req, res) => {
-  res.redirect('http://localhost:3002/arrows.png')
+  res.redirect('http://34.214.68.82/arrows.png')
 })
 
-app.get('/ratings/getData/1', (req,res) => {
-  res.redirect('http://localhost:3005/ratings/getData/1');
-})
 
-app.get('/about/getData/1', (req,res) => {
-  res.redirect('http://localhost:3003/about/getData/1')
-})
+// app.get('/earnings/getData', (req,res) => {
+//   res.redirect('http://localhost:3006/earnings/getData')
+// })
 
 app.get('/earnings/getData', (req,res) => {
-  res.redirect('http://localhost:3006/earnings/getData')
+  res.redirect('http://18.219.217.173/earnings/getData')
 })
 
-app.get('/news/getData', (req,res) => {
-  res.redirect('http://localhost:3004/news/getData')
-})
+// app.get('/graph/getStocks', (req, res) => {
+//   res.redirect('http://localhost:3001/graph/getStocks');
+// })
+
+// app.get('/questionMark.png', (req, res) => {
+//   res.redirect('http://localhost:3002/questionMark.png')
+// })
+
+// app.get('/exclamation.png', (req, res) => {
+//   res.redirect('http://localhost:3002/exclamation.png')
+// })
+
+// app.get('/arrows.png', (req, res) => {
+//   res.redirect('http://localhost:3002/arrows.png')
+// })
+
+// app.get('/ratings/getData/1', (req,res) => {
+//   res.redirect('http://localhost:3005/ratings/getData/1');
+// })
+
+// app.get('/about/getData/1', (req,res) => {
+//   res.redirect('http://localhost:3003/about/getData/1')
+// })
 
 
-app.post('/tradestock/user/deposit', (req,res) => {
-  res.redirect(307, 'http://localhost:3002/tradestock/user/deposit');
-})
+// app.get('/news/getData', (req,res) => {
+//   res.redirect('http://localhost:3004/news/getData')
+// })
+
+
+// app.post('/tradestock/user/deposit', (req,res) => {
+//   res.redirect(307, 'http://localhost:3002/tradestock/user/deposit');
+// })
 
 app.use(express.static('public'));
 
